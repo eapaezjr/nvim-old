2ae76b1061ca098f98626e5f4c86e7c6b7dad260
pzeadrian <pze.adrian@gmail.com>
2022-07-07 13:27:41 -0500

    Updating config for Linux

--- Linux/sync-script/.neovim.sh
+++ Linux/sync-script/.neovim.sh
@@ -5,7 +5,7 @@ cd
 cp  $HOME/.config/nvim/coc-settings.json $HOME/Documents/configNeovim/Linux/nvim/
 cp -R $HOME/.config/nvim/configs $HOME/Documents/configNeovim/Linux/nvim/
 cp  $HOME/.config/nvim/init.vim $HOME/Documents/configNeovim/Linux/nvim/
-cp $HOME/.neovim.sh $HOME/Documents/configNeovim/Linux/sync-script/
+#cp $HOME/.neovim.sh $HOME/Documents/configNeovim/Linux/sync-script/
 
 cd $HOME/Documents/configNeovim/
 git add -A
