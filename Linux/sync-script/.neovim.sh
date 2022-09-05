91d1c0fba8cbc87623d3aa01c998149e45e7d108
pzeadrian <pzeadrian@duck.com>
2022-09-05 02:04:39 -0500

    update

--- Linux/sync-script/.neovim.sh
+++ Linux/sync-script/.neovim.sh
@@ -2,7 +2,8 @@ cd $HOME/Documents/configNeovim/
 git pull
 cd
 
-cp -R $HOME/,config/nvim/* $HOME/Documents/configNeovim/Linux/nvim/
+cp -R $HOME/.config/nvim/configs $HOME/Documents/configNeovim/Linux/nvim/
+cp -R $HOME/.config/nvim/init.vim $HOME/Documents/configNeovim/Linux/nvim/
 
 cd $HOME/Documents/configNeovim/
 git add -A
